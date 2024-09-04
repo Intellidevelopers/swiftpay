461b4d297b4bf37ead49d1bf4cb7c7b9378862f8
Adeagbo Josiah <134317370+Intellidevelopers@users.noreply.github.com>
2024-09-04 18:58:06 +0100

    fff

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout() {
   useEffect(() => {
     if (loaded) {
       SplashScreen.hideAsync();
-      router.replace('/(tabs)');
+      router.replace('/splash');
     }
   }, [loaded]);
 
